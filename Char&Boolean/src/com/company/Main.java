8be346c7ed27648af98644c52b98d06c0c044a19
Valentin Mitroi <64599071+valentindaniel9926@users.noreply.github.com>
2021-04-05 08:19:49 +0300

    Add files via upload

---
+++ Char&Boolean/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	char Mychar = 'D';
+	char myUnicode= '\u0044';
+	char myNumber= '\u00A7';
+        System.out.println(myUnicode);
+        System.out.println(Mychar);
+        System.out.println(myNumber);
+
+        boolean myTrueBooleanValue= true;
+        boolean myFalseBooleanValue=false;
+        boolean isCustomerOverTwentyOne = true;
+
+    }
+}
